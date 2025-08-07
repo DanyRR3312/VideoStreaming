5de3e29afe38aba808088be680724544acaba35b
ElSucioDan <dany.r.r101@gmail.com>
2025-08-07 02:00:45 -0600

    actualizamos los enlaces de las imagenes

--- src/contentSlideInfo.js
+++ src/contentSlideInfo.js
@@ -3,28 +3,28 @@
 export const slideData = [
 
     {
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/InfoBanner/img1.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kaoru%20Hana%20wa%20Rin%20to%20Saku/assets/img1.webp',
         title: 'Kaoru Hana wa Rin to Saku',
         desc: 'La historia se centra en Rintaro Tsumugi, un estudiante de una escuela secundaria para chicos, y Kaoruko Waguri, una estudiante de una escuela secundaria para chicas cercana. A pesar de las diferencias entre sus escuelas y la hostilidad entre ellas, Rintaro y Kaoruko se enamoran y deben navegar por los desafíos de sus mundos separados',
         page: ''
     },
 
     {
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/InfoBanner/output.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kyoukai%20no%20Kanata/assets/img2.png',
         title: 'Kyoukai no Kanata',
         desc: 'La historia se centra en Akihito Kanbara, un estudiante de secundaria que es mitad Youmu e inmortal, y Mirai Kuriyama, una Guerrera del Mundo Espiritual con el poder de manipular la sangre. Sus vidas se entrelazan después de que Akihito salva a Mirai de suicidarse, desencadenando una serie de eventos que los obligan a enfrentar su destino.',
         page: ''
     },
 
     {
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/InfoBanner/img1.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kaoru%20Hana%20wa%20Rin%20to%20Saku/assets/img1.webp',
         title: 'Sousou no Frieren',
         desc: 'La elfa Frieren reflexiona sobre el valor del tiempo...',
         page: ''
     },
 
     {
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/InfoBanner/img1.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kaoru%20Hana%20wa%20Rin%20to%20Saku/assets/img1.webp',
         title: 'Blue Lock',
         desc: 'Yoichi Isagi entra en el experimento futbolístico más radical...',
         page: ''
--- src/recommendedData.js
+++ src/recommendedData.js
@@ -4,7 +4,7 @@ export const recommendedList = [
 
     {
         id: "kaoru-hana-wa-rin-to-saku",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kaoru%20Hana%20wa%20Rin%20to%20Saku/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kaoru%20Hana%20wa%20Rin%20to%20Saku/assets/descarga.webp',
         title: 'Kaoru Hana wa Rin to Saku',
         desc: 'La historia se centra en Rintaro Tsumugi, un estudiante de una escuela secundaria para chicos, y Kaoruko Waguri, una estudiante de una escuela secundaria para chicas cercana. A pesar de las diferencias entre sus escuelas y la hostilidad entre ellas, Rintaro y Kaoruko se enamoran y deben navegar por los desafíos de sus mundos separados',
         sub: 'Sub | Dob',
@@ -14,7 +14,7 @@ export const recommendedList = [
 
     {
         id: "kyoukai-no-kanata",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kyoukai%20no%20Kanata/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kyoukai%20no%20Kanata/assets/output3.webp',
         title: 'Kyoukai no Kanata',
         desc: 'La historia se centra en Akihito Kanbara, un estudiante de secundaria que es mitad Youmu e inmortal, y Mirai Kuriyama, una Guerrera del Mundo Espiritual con el poder de manipular la sangre. Sus vidas se entrelazan después de que Akihito salva a Mirai de suicidarse, desencadenando una serie de eventos que los obligan a enfrentar su destino.',
         sub: 'Sub | Dob',
@@ -24,7 +24,7 @@ export const recommendedList = [
 
     {
         id: "kyoukai-no-kanata",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kyoukai%20no%20Kanata/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kyoukai%20no%20Kanata/assets/output3.webp',
         title: 'Kyoukai no Kanata',
         desc: 'La historia se centra en Akihito Kanbara, un estudiante de secundaria que es mitad Youmu e inmortal, y Mirai Kuriyama, una Guerrera del Mundo Espiritual con el poder de manipular la sangre. Sus vidas se entrelazan después de que Akihito salva a Mirai de suicidarse, desencadenando una serie de eventos que los obligan a enfrentar su destino.',
         sub: 'Sub',
@@ -34,7 +34,7 @@ export const recommendedList = [
 
     {
         id: "kyoukai-no-kanata",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kyoukai%20no%20Kanata/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kyoukai%20no%20Kanata/assets/output3.webp',
         title: 'Kyoukai no Kanata',
         desc: 'La historia se centra en Akihito Kanbara, un estudiante de secundaria que es mitad Youmu e inmortal, y Mirai Kuriyama, una Guerrera del Mundo Espiritual con el poder de manipular la sangre. Sus vidas se entrelazan después de que Akihito salva a Mirai de suicidarse, desencadenando una serie de eventos que los obligan a enfrentar su destino.',
         sub: 'Sub',
@@ -44,7 +44,7 @@ export const recommendedList = [
 
     {
         id: "kyoukai-no-kanata",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kyoukai%20no%20Kanata/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kyoukai%20no%20Kanata/assets/output3.webp',
         title: 'Kyoukai no Kanata',
         desc: 'La historia se centra en Akihito Kanbara, un estudiante de secundaria que es mitad Youmu e inmortal, y Mirai Kuriyama, una Guerrera del Mundo Espiritual con el poder de manipular la sangre. Sus vidas se entrelazan después de que Akihito salva a Mirai de suicidarse, desencadenando una serie de eventos que los obligan a enfrentar su destino.',
         sub: 'Sub | Dob',
@@ -54,7 +54,7 @@ export const recommendedList = [
 
     {
         id: "kyoukai-no-kanata",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kyoukai%20no%20Kanata/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kyoukai%20no%20Kanata/assets/output3.webp',
         title: 'Kyoukai no Kanata',
         desc: 'La historia se centra en Akihito Kanbara, un estudiante de secundaria que es mitad Youmu e inmortal, y Mirai Kuriyama, una Guerrera del Mundo Espiritual con el poder de manipular la sangre. Sus vidas se entrelazan después de que Akihito salva a Mirai de suicidarse, desencadenando una serie de eventos que los obligan a enfrentar su destino.',
         sub: 'Sub | Dob',
@@ -64,7 +64,7 @@ export const recommendedList = [
 
     {
         id: "kyoukai-no-kanata",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kyoukai%20no%20Kanata/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kyoukai%20no%20Kanata/assets/output3.webp',
         title: 'Kyoukai no Kanata',
         desc: 'La historia se centra en Akihito Kanbara, un estudiante de secundaria que es mitad Youmu e inmortal, y Mirai Kuriyama, una Guerrera del Mundo Espiritual con el poder de manipular la sangre. Sus vidas se entrelazan después de que Akihito salva a Mirai de suicidarse, desencadenando una serie de eventos que los obligan a enfrentar su destino.',
         sub: 'Sub',
@@ -74,7 +74,7 @@ export const recommendedList = [
 
     {
         id: "kaoru-hana-wa-rin-to-saku",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kaoru%20Hana%20wa%20Rin%20to%20Saku/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kaoru%20Hana%20wa%20Rin%20to%20Saku/assets/descarga.webp',
         title: 'Kaoru Hana wa Rin to Saku',
         desc: 'La historia se centra en Rintaro Tsumugi, un estudiante de una escuela secundaria para chicos, y Kaoruko Waguri, una estudiante de una escuela secundaria para chicas cercana. A pesar de las diferencias entre sus escuelas y la hostilidad entre ellas, Rintaro y Kaoruko se enamoran y deben navegar por los desafíos de sus mundos separados',
         sub: 'Sub',
@@ -84,7 +84,7 @@ export const recommendedList = [
 
     {
         id: "kaoru-hana-wa-rin-to-saku",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kaoru%20Hana%20wa%20Rin%20to%20Saku/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kaoru%20Hana%20wa%20Rin%20to%20Saku/assets/descarga.webp',
         title: 'Kaoru Hana wa Rin to Saku',
         desc: 'La historia se centra en Rintaro Tsumugi, un estudiante de una escuela secundaria para chicos, y Kaoruko Waguri, una estudiante de una escuela secundaria para chicas cercana. A pesar de las diferencias entre sus escuelas y la hostilidad entre ellas, Rintaro y Kaoruko se enamoran y deben navegar por los desafíos de sus mundos separados',
         sub: 'Sub | Dob',
@@ -94,7 +94,7 @@ export const recommendedList = [
 
     {
         id: "kaoru-hana-wa-rin-to-saku",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kaoru%20Hana%20wa%20Rin%20to%20Saku/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kaoru%20Hana%20wa%20Rin%20to%20Saku/assets/descarga.webp',
         title: 'Kaoru Hana wa Rin to Saku',
         desc: 'La historia se centra en Rintaro Tsumugi, un estudiante de una escuela secundaria para chicos, y Kaoruko Waguri, una estudiante de una escuela secundaria para chicas cercana. A pesar de las diferencias entre sus escuelas y la hostilidad entre ellas, Rintaro y Kaoruko se enamoran y deben navegar por los desafíos de sus mundos separados',
         sub: 'Sub | Dob',
@@ -104,7 +104,7 @@ export const recommendedList = [
 
     {
         id: "kaoru-hana-wa-rin-to-saku",
-        img: 'https://PreguntasPorMateria-ElsucioDan.b-cdn.net/VideoStreaming/assets/ListingsSeries/Kaoru%20Hana%20wa%20Rin%20to%20Saku/thumbnail.webp',
+        img: 'https://pruebas-assets-videostreaming.b-cdn.net/VideoStreaming/series/Kaoru%20Hana%20wa%20Rin%20to%20Saku/assets/descarga.webp',
         title: 'Kaoru Hana wa Rin to Saku',
         desc: 'La historia se centra en Rintaro Tsumugi, un estudiante de una escuela secundaria para chicos, y Kaoruko Waguri, una estudiante de una escuela secundaria para chicas cercana. A pesar de las diferencias entre sus escuelas y la hostilidad entre ellas, Rintaro y Kaoruko se enamoran y deben navegar por los desafíos de sus mundos separados',
         sub: 'Sub',
